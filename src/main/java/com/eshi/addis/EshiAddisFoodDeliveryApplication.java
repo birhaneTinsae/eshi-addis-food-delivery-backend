61ec1ef813d07dfb5e7bb4e5bb122c8426d7ffa0
Birhane Tinsae <birhane.tinsaa@gmail.com>
2020-03-27 22:23:38 +0300

    add storage handler

--- src/main/java/com/eshi/addis/EshiAddisFoodDeliveryApplication.java
+++ src/main/java/com/eshi/addis/EshiAddisFoodDeliveryApplication.java
@@ -1,9 +1,13 @@
 package com.eshi.addis;
 
+import com.eshi.addis.storage.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class EshiAddisFoodDeliveryApplication {
 
     public static void main(String[] args) {
